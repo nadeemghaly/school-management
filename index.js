08a38409f7458f02cc40df10ce61ea302120b19e
Nadeem Ghaly <nadeemghaly@gmail.com>
2024-07-09 20:27:18 +0300

    fix login not owrking

--- index.js
+++ index.js
@@ -1,39 +1,28 @@
 const config                = require('./config/index.config.js');
+const Cortex                = require('ion-cortex');
 const ManagersLoader        = require('./loaders/ManagersLoader.js');
 
-process.on('uncaughtException', err => {
-    console.log(`Uncaught Exception:`)
-    console.log(err, err.stack);
+const mongoDB = config.dotEnv.MONGO_URI? require('./connect/mongo')({
+    uri: config.dotEnv.MONGO_URI
+}):null;
 
-    process.exit(1)
-})
-
-process.on('unhandledRejection', (reason, promise) => {
-    console.log('Unhandled rejection at ', promise, `reason:`, reason);
-    process.exit(1)
-})
-
-// const cache      = require('./cache/cache.dbh')({
+// const cache = require('./cache/cache.dbh')({
 //     prefix: config.dotEnv.CACHE_PREFIX ,
 //     url: config.dotEnv.CACHE_REDIS
 // });
 
-// const Oyster  = require('oyster-db');
-// const oyster     = new Oyster({ 
-//     url: config.dotEnv.OYSTER_REDIS, 
-// 	prefix: config.dotEnv.OYSTER_PREFIX 
-// });
+// // const cortex = new Cortex({
+// //     prefix: config.dotEnv.CORTEX_PREFIX,
+// //     url: config.dotEnv.CORTEX_REDIS,
+// //     type: config.dotEnv.CORTEX_TYPE,
+// //     state: ()=>{
+// //         return {} 
+// //     },
+// //     activeDelay: "50ms",
+// //     idlDelay: "200ms",
+// // });
 
-// const cortex     = new Cortex({
-//     prefix: config.dotEnv.CORTEX_PREFIX,
-//     url: config.dotEnv.CORTEX_REDIS,
-//     type: config.dotEnv.CORTEX_TYPE,
-//     state: ()=>{
-//         return {} 
-//     },
-//     activeDelay: "50",
-//     idlDelay: "200",
-// });
+
 
 const managersLoader = new ManagersLoader({config});
 const managers = managersLoader.load();
